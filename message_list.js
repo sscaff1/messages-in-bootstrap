78f504d3b3ad6fb8cf969b3f37a9f16dd25b0bf5
Steven Scaffidi <sscaff1@gmail.com>
2015-09-07 21:28:48 -0500

    fixing the package

--- message_list.js
+++ message_list.js
@@ -15,9 +15,9 @@ Template.messageList.helpers({
 	}
 });
 
-Template.messageItem.rendered = function() {
+Template.messageItem.onRendered(function() {
   var messageCurrent = this.data;
   Meteor.setTimeout(function () {
     Messages.collection.remove(messageCurrent._id);
   }, 3000);
-};
+});
--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A simple package to display success and error messages to the user',
   // URL to the Git repository containing the source code for this package.
- // git: '',
+  git: 'https://github.com/sscaff1/messages-in-bootstrap',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -20,11 +20,18 @@ Package.onUse(function(api) {
   ], 'client');
 
   api.addFiles([
-    'message_list.js',
+    'message_list.css',
     'message_list.html',
+    'message_list.js',
   ], 'client');
 
   if (api.export) {
     api.export('Messages');
   }
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('sscaff1:messages');
+  api.addFiles('messages_test.js');
 });

b367268083c6f0aef2ed2ad820d32ca1a838e298
Steven Scaffidi <sscaff1@gmail.com>
2015-09-17 21:51:35 -0500

    fixing CSS

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sscaff1:messages',
-  version: '0.0.5',
+  version: '0.0.6',
   // Brief, one-line summary of the package.
   summary: 'A simple package to display success and error messages to the user',
   // URL to the Git repository containing the source code for this package.
